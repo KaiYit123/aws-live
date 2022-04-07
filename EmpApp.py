6008c742e2eac3ce78d78985ae4988d3a7c9294b
KaiYit123 <86282989+KaiYit123@users.noreply.github.com>
2022-04-07 15:05:54 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -62,9 +62,9 @@ def update():
     pri_skill = request.form['pri_skill']
     location = request.form['location']
 
-    up_sql = "UPDATE employee SET first_name=KY, last_name=HONG, pri_skill=%s,location=%s WHERE emp_id = %s"
+    up_sql = "UPDATE employee SET first_name=%s, last_name=%s, pri_skill=%s,location=%s WHERE emp_id = %s"
     cursor = db_conn.cursor()
-    cursor.execute(up_sql,(first_name,last_name,pri_skill,location,emp_id))
+    cursor.execute(up_sql,(KY,HONG,pri_skill,location,emp_id))
     db_conn.commit()
     user = cursor.fetchone()
     cursor.close()
